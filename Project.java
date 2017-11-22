48cc28943cb072a556f38659164cc306585ada41
nickmilly <33884951+nickmilly@users.noreply.github.com>
2017-11-21 23:10:00 -0400

    Update Project.java

--- Project.java
+++ Project.java
@@ -13,63 +13,60 @@ public class Project {
     }
     
     
-    public static String Location(){
+    public static String Location(int roundNum){
         int randPlace = 5; // variable for which location is randomly selected
         String location = "";
         Random rand = new Random();
         randPlace = rand.nextInt(5);
-        if (randPlace == 0) {
-            location = "warehouse";
+        if (roundNum==1) { // locations for first round
+            if (randPlace == 0) {
+                location = "Moscow";
+            }
+            if (randPlace == 1) { 
+                location = "Shanghai";
+            }   
+            if (randPlace == 2) {
+                location = "London";
+            }
+            if (randPlace == 3) {
+                location = "Copenhagen";
+            }
+            if (randPlace == 4) {
+                location = "Washington";
+            }
         }
-        if (randPlace == 1) { 
-            location = "embassy";
-        }
-        if (randPlace == 2) {
-            location = "skyscraper";
-        }
-        if (randPlace == 3) {
-            location = "parliment";
-        }
-        if (randPlace == 4) {
-            location = "catacombs";
+        else if (roundNum==2) { // locations for second round
+            if (randPlace == 0) {
+                location = "warehouse";
+            }
+            if (randPlace == 1) { 
+                location = "embassy";
+            }
+            if (randPlace == 2) {
+                location = "skyscraper";
+            }
+            if (randPlace == 3) {
+               location = "parliment";
+            }
+            if (randPlace == 4) {
+                location = "catacombs";
+            }
         }
         return location;
-    }
-    
-    public static String City(){
-        int randCity = 5; // variable for which city is randomly chosen
-        String city = "";
-        Random rand = new Random();
-        randPlace = rand.nextInt(5);
-        if (randCity == 0) {
-            city = "Moscow";
-        }
-        if (randCity == 1) { 
-            city = "Shanghai";
-        }
-        if (randCity == 2) {
-            city = "London";
-        }
-        if (randCity == 3) {
-            city = "Copenhagen";
-        }
-        if (randCity == 4) {
-            city = "Washington";
-        }
-        return city;
         
     }
     
-
+    
     public static void Easy() {
         Random rand = new Random();
         Scanner kb = new Scanner(System.in);
+        int roundNum = 1; //  counter for what round is the user on
         System.out.println("Decode the secret message below!\n");
-        String city = City(); // city is the retunred city from the City method
+        String location = Location(roundNum); // String location is the retunred location from the method: Location 
         boolean keepPlaying = true; // keeps track if the user still wats to play
         int roundProgress = 1; // changes the difficulty as more words are found
         //while (keepPlaying=true) {
-        for (int i=0; i<city.length(); i++) {
+        for (int i=0; i<location.length(); i++) {
             int num1, num2, addSub; // addSub decides if question will be addition or subtraction
             num1 = rand.nextInt(10 * roundProgress); // num randomly chosen. 1 didget if round 1, 2 didget if round 2, etc
             num2 = rand.nextInt(10 * roundProgress); // num randomly chosen. 1 didget if round 1, 2 didget if round 2, etc
@@ -79,21 +76,21 @@ public class Project {
                 if (addSub == 0) { // this question is addition
                     System.out.print(num1 + " + " + num2 + " = ");
                     int answer = kb.nextInt();
-                    if (answer == num1+num2 && i == city.length() - 1) { // when the last letter is found
-                        System.out.print(i+1 + "/" + city.length() + " of the message decoded! ");
-                        System.out.print("You found the letter " + city.charAt(i));
+                    if (answer == num1+num2 && i == location.length() - 1) { // when the last letter is found
+                        System.out.print(i+1 + "/" + location.length() + " of the message decoded! ");
+                        System.out.print("You found the letter " + location.charAt(i));
                         System.out.println(". You found the last one!\n");
                         correct = true; // the math question was solved correctly
                  }
-                    else if (answer == num1+num2 && i >= city.length() - 3) { // when the word is almost solved
-                        System.out.print(i+1 + "/" + city.length() + " of the job done! ");
-                        System.out.print("You found the letter " + city.charAt(i));
+                    else if (answer == num1+num2 && i >= location.length() - 3) { // when the word is almost solved
+                        System.out.print(i+1 + "/" + location.length() + " of the job done! ");
+                        System.out.print("You found the letter " + location.charAt(i));
                         System.out.println(". We're really close now!\n");
                         correct = true; // the math question was solved correctly
                     }
                     else if (answer == num1+num2) { // when a letter is found, but word not almost solved
-                        System.out.print(i+1 + "/" + city.length() + " parts done! ");
-                        System.out.print("You found the letter " + city.charAt(i));
+                        System.out.print(i+1 + "/" + location.length() + " parts done! ");
+                        System.out.print("You found the letter " + location.charAt(i));
                         System.out.println(". Good work! On to the next one:\n");
                         correct = true; // the math question was solved correctly
                     }
@@ -105,21 +102,21 @@ public class Project {
                 else { // this question is subtraction
                     System.out.print(num1 + " - " + num2 + " = ");
                     int answer = kb.nextInt();
-                    if (answer == num1-num2 && i == city.length() - 1) { // when the last letter is found
-                        System.out.print(i+1 + "/" + city.length() + " of the message decoded! ");
-                        System.out.print("You found the letter " + city.charAt(i));
+                    if (answer == num1-num2 && i == location.length() - 1) { // when the last letter is found
+                        System.out.print(i+1 + "/" + location.length() + " of the message decoded! ");
+                        System.out.print("You found the letter " + location.charAt(i));
                         System.out.println(". You found the last one!\n");
                         correct = true; // the math question was solved correctly
                  }
-                    else if (answer == num1-num2 && i >= city.length() - 3) { // when the word is almost solved
-                        System.out.print(i+1 + "/" + city.length() + " of the job done! ");
-                        System.out.print("You found the letter " + city.charAt(i));
+                    else if (answer == num1-num2 && i >= location.length() - 3) { // when the word is almost solved
+                        System.out.print(i+1 + "/" + location.length() + " of the job done! ");
+                        System.out.print("You found the letter " + location.charAt(i));
                         System.out.println(". We're really close now!\n");
                         correct = true; // the math question was solved correctly
                     }
                     else if (answer == num1-num2) { // when a letter is found, but word not almost solved
-                        System.out.print(i+1 + "/" + city.length() + " parts done! ");
-                        System.out.print("You found the letter " + city.charAt(i));
+                        System.out.print(i+1 + "/" + location.length() + " parts done! ");
+                        System.out.print("You found the letter " + location.charAt(i));
                         System.out.println(". Good work! On to the next one:\n");
                         correct = true; // the math question was solved correctly
                     }
@@ -128,9 +125,11 @@ public class Project {
                     }
                 }
             }
-        roundProgess *= 10;
         }
-        System.out.println("You decoded the word " + city +"!");
+        System.out.println("You decoded the word " + location +"!");
+        
+        roundNum ++; // round goes up to next round
+        roundProgress *= 10; // next round uses 2 didget addition and subtraction
         //}
 
     }
